5261ee89e69f59ee32fafd7a1eb9fc70f30a09fe
Muhframos <pacocacombinacomtudo@gmail.com>
2021-04-06 14:24:37 -0300

    cliente

--- config/mod-config.js
+++ config/mod-config.js
@@ -4,36 +4,5 @@ customTiers - these are auto-detected by the script, but you can set them here t
 excludeStandardTiers - set to true if you want only your custom tiers to show up for the format
 */
 const ModConfigData = {
-	ClientMods: {
-		cleanslate: {
-			excludeStandardTiers: true,
-		},
-		cleanslatemicro: {
-			excludeStandardTiers: true,
-		},
-		csts: {
-			customTiers: ['CS1', 'CSM', 'CS2'],
-			excludeStandardTiers: true,
-		},
-		roulettemons: {
-			excludeStandardTiers: true,
-		},
-		ccapm2020: {
-			excludeStandardTiers: true,
-		},
-		fealpha: {
-			excludeStandardTiers: true,
-		},
-		feuu: {
-			excludeStandardTiers: true,
-		},
-		prism: {
-			ignoreEVLimits: true,
-			spriteGen: 2,
-		},
-		smashmodsmelee: {
-			excludeStandardTiers: true,
-		},
-	},
 };
 exports.ModConfigData = ModConfigData;
