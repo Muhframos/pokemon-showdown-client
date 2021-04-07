14706abce730384fbe391d5afc6d7e40ca48bdd7
Muhframos <pacocacombinacomtudo@gmail.com>
2021-04-07 03:01:52 -0300

    e

--- config/config.js
+++ config/config.js
@@ -18,8 +18,8 @@ Config.whitelist = [
 // `defaultserver` specifies the server to use when the domain name in the
 // address bar is `Config.routes.client`.
 Config.defaultserver = {
-	id: 'adventures',
-	host: '24.184.93.50.psim.us',
+	id: 'showdown',
+	host: 'sim3.psim.us',
 	port: 443,
 	httpport: 8000,
 	altport: 80,
@@ -37,7 +37,7 @@ Config.version = "0.11.2 (6f8a8dd9/b952f1af)";
 
 Config.routes = {
 	root: 'pokemonshowdown.com',
-	client: 'adventures-showdown.herokuapp.com',
+	client: '24.184.93.50.psim.us',
 	dex: 'dex.pokemonshowdown.com',
 	replays: 'replay.pokemonshowdown.com',
 	users: 'pokemonshowdown.com/users',
